f468983b381d9754025fbb3e120ed50cfa8a82ed
Tallerik <erik.klasse@gmail.com>
2019-06-10 18:33:23 +0200

    Added TODO for @MayusYT

--- src/main/java/net/snapecraft/vote/Vote.java
+++ src/main/java/net/snapecraft/vote/Vote.java
@@ -8,6 +8,7 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
@@ -28,6 +29,11 @@ public final class Vote extends Plugin implements Listener {
     @EventHandler
     public void vote(VotifierEvent event) {
         vote(event.getVote());
+    }
+
+    @EventHandler
+    public void join(LoginEvent event) {
+        //TODO: Check if player has vote in database -> MySQL
     }
 
     private void vote(com.vexsoftware.votifier.model.Vote vote) {
